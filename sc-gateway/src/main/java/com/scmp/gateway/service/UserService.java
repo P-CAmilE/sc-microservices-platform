e0dc2e5383ad3eaf620cc37d91b57044483d2287
P-CAmilE <43110057+P-CAmilE@users.noreply.github.com>
2020-08-13 18:57:24 +0800

    add sentinel

--- sc-gateway/src/main/java/com/scmp/gateway/service/UserService.java
+++ sc-gateway/src/main/java/com/scmp/gateway/service/UserService.java
@@ -2,12 +2,12 @@ package com.scmp.gateway.service;
 
 import com.scmp.domain.ResultVO;
 import com.scmp.domain.User;
+import com.scmp.gateway.service.fallback.UserServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-user")
-//@FeignClient(value = "service-user", fallback = )
+@FeignClient(value = "service-user", fallback = UserServiceFallback.class)
 public interface UserService {
 
     @GetMapping("/user/VerifyToken")
